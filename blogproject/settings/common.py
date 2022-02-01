01010b51dd4fa5a55d733d7a6cf4cbd6293cd029
latteplus <latteplus@163.com>
2022-02-01 16:10:33 +0800

    fabfile

--- blogproject/settings/common.py
+++ blogproject/settings/common.py
@@ -13,19 +13,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^6!h$bm^z62j%98q_e5yc90z(+!!3o8enj*=rq%c3!z_y3559)'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".my-django-blog.com", "192.168.80.200:8000", "192.168.80.200"]
 
 
 # Application definition
---
+++ blogproject/settings/local.py
@@ -0,0 +1,11 @@
+from .common import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'development-key'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+ALLOWED_HOSTS = ['*']
