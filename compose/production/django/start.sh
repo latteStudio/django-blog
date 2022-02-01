03c2084e7656af091cf72bf0fbefaedf2f59eb8c
latteplus <latteplus@163.com>
2022-02-01 21:37:10 +0800

    docker build

---
+++ compose/production/django/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+gunicorn blogproject.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --chdir=/app
+# 启动gunicorn进程，
