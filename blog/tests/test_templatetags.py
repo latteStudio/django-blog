bed223ddb4f002b9a3281bf0a6db0292b881142d
latteplus <latteplus@163.com>
2022-02-06 11:54:53 +0800

    1

---
+++ blog/tests/test_templatetags.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.apps import apps
+from django.contrib.auth.models import User
+from blog.models import Post, Category, Tag
+from django.utils import timezone
+from datetime import timedelta
+from blog.feed import AllPostsRssFeed
+from django.template import Template, Context
+
+
+class TemplateTagsTestCase(TestCase):
+    def setUp(self):
+        apps.get_app_config('haystack').signal_processor.teardown()
+
+        self.user = User.objects.create_superuser(
+            username='admin',
+            email='admin@admin.com',
+            password='admin',
+        )
+
+        self.cate = Category.objects.create(
+            name='cate1'
+        )
+
+    def test_show_recent_posts_with_posts(self):
+        post = Post.objects.create(
+            title='测试标题',
+            body='测试内容',
+            category=self.cate,
+            author=self.user,
+        )
+        context = Context(show_recently_post(self.ctx))
+        template = Template(
+            '{% load blog_extras %}'
+            '{% show_recently_post %}'
+        )
+        expected_html = template.render(context)
+        self.assertInHTML('<h3 class="widget-title">最新文章</h3>', expected_html)
+        self.assertInHTML('<a href="{}">{}</a>'.format(post.get_absolute_url(), post.title), expected_html)
