fcf84cb6743b46c8b9765535ffc3805e82a38cc2
latteplus <latteplus@163.com>
2022-02-06 11:41:36 +0800

    1

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from blog.models import Post, Category, Tag
 from django.utils import timezone
 from datetime import timedelta
-
+from blog.feed import AllPostsRssFeed
 
 """
 思路：
@@ -92,3 +92,84 @@ class CategoryViewTestCase(BlogDataTestCase):
         self.assertEqual(res.context['post_list'].count(), 1)
         expected_qs = self.cate1.post_set.all().order_by('-created_time')
         self.assertQuerysetEqual(res.context['post_list'], [repr(p) for p in expected_qs])
+
+
+class PostDetailViewTestCase(BlogDataTestCase):
+    def setUp(self):
+        super(PostDetailViewTestCase, self).setUp()
+        self.md_post = Post.objects.create(
+            title='md测试',
+            body='# 一级标题',
+            category=self.cate1,
+            user=self.user,
+        )
+        self.url = reverse('blog:detail', kwargs={'pk': self.md_post.pk})
+
+    def test_good_view(self):
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed('blog/detail.html')
+        self.assertContains(res, self.md_post.title)
+        self.assertIn('post', res.context)
+
+    def test_visit_a_noneexist_post(self):
+        url = reverse('blog:detail', kwargs={'pk': 100})
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 404)
+
+    def test_increase_views(self):
+        self.client.get(self.url)
+        self.md_post.refresh_from_db()
+        self.assertEqual(self.md_post.views, 1)
+
+        self.client.get(self.url)
+        self.md_post.refresh_from_db()
+        self.assertEqual(self.md_post.views, 2)
+
+    def test_markdownify_post_body_and_set_toc(self):
+        res = self.client.get(self.url)
+        self.assertContains(res, '文章目录')
+        self.assertContains(res, self.md_post.title)
+
+        post_template_var = res.context['post']
+        self.assertHTMLEqual(post_template_var.body_html, "<h1 id='标题'>标题</h1>")
+        self.assertHTMLEqual(post_template_var.toc, '<li><a href="#标题">一级标题</li>')
+
+
+
+class AdminTestCase(BlogDataTestCase):
+    def setUp(self):
+        super(AdminTestCase, self).setUp()
+        self.url = reverse('admin:blog_post_add')
+
+    def test_set_author_after_pub_the_post(self):
+        data = {
+            'title': '测试',
+            'body': '测试内容',
+            'category': self.cate1.pk,
+        }
+
+        self.client.login(username=self.user.username, password='admin')
+        res = self.client.post(self.url, data=data)
+        self.assertEqual(res.status_code, 200)
+        post = Post.objects.all().latest('created_time')
+        self.assertEqual(post.user, self.user)
+        self.assertEqual(post.title, data.get('title'))
+        self.assertEqual(post.category, self.cate1)
+
+
+class RSSTestCase(BlogDataTestCase):
+    def setUp(self):
+        super(RSSTestCase, self).setUp()
+        self.url  = reverse('rss')
+
+    def test_rss_subrss_content(self):
+        response = self.client.get(self.url)
+        self.assertContains(response, AllPostsRssFeed.title)
+        self.assertContains(response, AllPostsRssFeed.description)
+        self.assertContains(response, self.post1.title)
+        self.assertContains(response, self.post2.title)
+        self.assertContains(response, '[%s] %s' % (self.post1.category, self.post1.title))
+        self.assertContains(response, '[%s] %s' % (self.post2.category, self.post2.title))
+        self.assertContains(response, self.post1.body)
+        self.assertContains(response, self.post2.body)
